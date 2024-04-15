fc1d9d70250d488fa44113411536fb08003e9721
wsdt <kevin.riedl@wavect.io>
2024-04-15 17:18:44 +0200

    chore: Init

---
+++ contracts/YourContract.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.9;
+
+contract YourContract {
+    uint256 public value = 123;
+
+    function setValue(uint256 _value) public {
+        value = _value;
+    }
+}
---
+++ deploy/contract.deploy.ts
@@ -0,0 +1,46 @@
+import {Contract, ethers, Wallet} from 'ethers'
+import YourContractJson from "../artifacts/contracts/YourContract.sol/YourContract.json";
+
+let YourContract: Contract
+
+const hre = require("hardhat")
+const prompt = require("prompt-sync")()
+
+async function main() {
+
+    console.log(`'Deploying Light bridge contract...`)
+
+    const provider = hre.ethers.provider
+    const network = hre.network
+
+    console.log(`Network name=${network?.name}`)
+    let fileName
+
+    const deployer = new Wallet(network.config.accounts[0], provider)
+    console.log(`Network, ${network?.name}`)
+
+    const Factory__YourContract = new ethers.ContractFactory(
+        YourContractJson.abi,
+        YourContractJson.bytecode,
+        deployer
+    )
+
+    let gasLimit = prompt("Custom gas limit? [number/N]")
+    if (isNaN(gasLimit?.toLowerCase())) {
+        gasLimit = null;
+    } else {
+        gasLimit = parseInt(gasLimit)
+    }
+
+    YourContract = await Factory__YourContract.deploy({gasLimit})
+    let res = await YourContract.deployTransaction.wait()
+    console.log(`Deployed contract: `, res)
+
+    console.log(`Contract deployed to: ${YourContract.address}`)
+}
+
+main().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+});
+
